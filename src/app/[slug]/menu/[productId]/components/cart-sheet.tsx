5ba3434016bd16d846d42ce1a5ff7eb3dc211a47
FellypeGC <gabrielfellypesteve@gmail.com>
2025-02-24 18:40:00 -0300

    feat: ensure cart total is shown

--- src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
+++ src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
@@ -1,22 +1,36 @@
 "use client"
 
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart";
 import CartProductItem from "../../components/cart-product-item";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { formatCurrency } from "@/helpers/format-currency";
 
 const CartSheet = () => {
-  const { isOpen, toggleCart, products } = useContext(CartContext);
+  const { isOpen, toggleCart, products, total } = useContext(CartContext);
   return (
     <Sheet open={isOpen} onOpenChange={toggleCart}>
       <SheetContent className="w-[80%]">
         <SheetHeader>
           <SheetTitle className="text-left">Sacola</SheetTitle>
         </SheetHeader>
-        <div className="pt-5">
-          {products.map((product) => (
-            <CartProductItem key={product.id} product={product} />
-          ))}
+        <div className="py-5 flex flex-col h-full">
+          <div className="flex-auto">
+            {products.map((product) => (
+              <CartProductItem key={product.id} product={product} />
+            ))}
+          </div>
+          <Card className="mb-6">
+            <CardContent className="p-5">
+              <div className="flex justify-between">
+                <p className="text-sm text-muted-foreground">Total</p>
+                <p className="font-semibold text-sm">{formatCurrency(total)}</p>
+              </div>
+            </CardContent>
+          </Card>
+          <Button className="w-full rounded-full">Finalizar pedido</Button>
         </div>
       </SheetContent>
     </Sheet>
