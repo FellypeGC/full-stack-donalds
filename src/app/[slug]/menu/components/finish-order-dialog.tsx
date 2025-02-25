f7e3399eb6a2d638b222ef9697493b830e37a658
FellypeGC <gabrielfellypesteve@gmail.com>
2025-02-24 21:25:04 -0300

    feat: ensure order can be created

--- src/app/[slug]/menu/components/finish-order-dialog.tsx
+++ src/app/[slug]/menu/components/finish-order-dialog.tsx
@@ -28,6 +28,13 @@ import {
 } from "@/components/ui/form";
 
 import { Input } from "@/components/ui/input";
+import { createOrder } from "../actions/create-order";
+import type { ConsumptionMethod } from "@prisma/client";
+import { useParams, useSearchParams } from "next/navigation";
+import { useContext, useTransition } from "react";
+import { CartContext } from "../contexts/cart";
+import { toast } from "sonner";
+import { Loader2Icon } from "lucide-react";
 
 const formSchema = z.object({
   name: z.string().trim().min(1, {
@@ -48,6 +55,11 @@ interface FinishOrderDialogProps {
 }
 
 const FinishOrderDialog = ({ open, onOpenChange }: FinishOrderDialogProps) => {
+  const { slug } = useParams<{ slug: string }>()
+  const { products } = useContext(CartContext);
+  const searchParams = useSearchParams();
+  const [isPending, startTransition] = useTransition()
+
   const form = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -56,14 +68,30 @@ const FinishOrderDialog = ({ open, onOpenChange }: FinishOrderDialogProps) => {
     },
     shouldUnregister: true,
   });
-  const onSubmit = (data: FormSchema) => {
-    console.log({ data });
+
+  const onSubmit = async (data: FormSchema) => {
+    try {
+      const consumptionMethod = searchParams.get("consumptionMethod") as ConsumptionMethod;
+      startTransition( async () => {
+        await createOrder({
+          consumptionMethod,
+          customerCpf: data.cpf,
+          customerName: data.name,
+          products,
+          slug,
+        });
+        onOpenChange(false);
+        toast.success("Pedido finalizado com sucesso!");
+      });
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return ( 
     <Drawer open={open} onOpenChange={onOpenChange}>
       <DrawerTrigger asChild>
-        
+
       </DrawerTrigger>
       <DrawerContent>
         <DrawerHeader>
@@ -111,7 +139,9 @@ const FinishOrderDialog = ({ open, onOpenChange }: FinishOrderDialogProps) => {
                 type="submit" 
                 variant="destructive"
                 className="rounded-full"
+                disabled={isPending}
               >
+                {isPending && <Loader2Icon className="animate-spin" />}
                 Finalizar
               </Button>
                 <DrawerClose>
--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -2,7 +2,7 @@ import { formatCurrency } from "@/helpers/format-currency";
 import type { Product } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, useSearchParams } from "next/navigation";
 
 interface ProductsProps {
   products: Product[];
@@ -10,11 +10,17 @@ interface ProductsProps {
 
 const Products = ({ products }: ProductsProps) => {
   const { slug } = useParams<{ slug: string }>();
+  const searchParams = useSearchParams();
+  const consumptionMethod = searchParams.get('consumptionMethod');
 
   return (
     <div className="space-y-3 px-1.5 py-3">
       {products.map(product => (
-        <Link key={product.id} href={`/${slug}/menu/${product.id}`} className="flex items-center justify-between gap-10 py-3 border-b">
+        <Link 
+          key={product.id} 
+          href={`/${slug}/menu/${product.id}?consumptionMethod=${consumptionMethod}`} 
+          className="flex items-center justify-between gap-10 py-3 border-b"
+        >
           <div>
             <h3 className="text-sm font-medium">{product.name}</h3>
             <p className="line-clamp-2 text-sm text-muted-foreground">{product.description}</p>
